45979c7cf819619db5695ddbe7984daaf24d6b1c
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-05-03 21:49:36 +0200

    Updated GeneratingQuestionnaire

--- Assets/VRSTK/Scripts/Questionnaires/GenerateQuestionnaireEditor.cs
+++ Assets/VRSTK/Scripts/Questionnaires/GenerateQuestionnaireEditor.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System;
 using VRSTK.Scripts.Telemetry;
 using VRSTK.Scripts.Playback;
+using VRQuestionnaireToolkit;
 
 namespace VRSTK
 {
@@ -16,10 +17,10 @@ namespace VRSTK
             public class GenerateQuestionnaireEditor : EditorWindow
             {
 
-                public GameObject trackedObject;
-                public GameObject lastTrackedObject;
-                private bool[] trackedComponents;
-                private bool[][] trackedVariables;
+                public GameObject _questionnaireToolkit;
+                public GameObject _lastQuestionnaireToolkit;
+                //private bool[] _trackedComponents;
+                //private bool[][] _trackedVariables;
 
                 [MenuItem("Window/VRSTK/Generate Questionnaire")]
                 public static void ShowWindow()
@@ -29,19 +30,19 @@ namespace VRSTK
 
                 void OnEnable()
                 {
-                    trackedObject = Selection.activeGameObject;
-                    if (lastTrackedObject == null && trackedObject != null)
+                    _questionnaireToolkit = Selection.activeGameObject;
+                    if (_lastQuestionnaireToolkit == null && _questionnaireToolkit != null)
                     {
-                        trackedComponents = new bool[trackedObject.GetComponents(typeof(Component)).Length];
-                        trackedVariables = new bool[trackedObject.GetComponents(typeof(Component)).Length][];
+                        //_trackedComponents = new bool[_questionnaireToolkit.GetComponents(typeof(Component)).Length];
+                        //_trackedVariables = new bool[_questionnaireToolkit.GetComponents(typeof(Component)).Length][];
                         //lastTrackedObject = trackedObject;
                     }
                 }
 
                 private void OnInspectorUpdate()
                 {
-                    trackedObject = Selection.activeGameObject;
-                    if (trackedObject != lastTrackedObject)
+                    _questionnaireToolkit = Selection.activeGameObject;
+                    if (_questionnaireToolkit != _lastQuestionnaireToolkit)
                     {
                         Repaint();
                     }
@@ -49,57 +50,58 @@ namespace VRSTK
 
                 private void OnGUI()
                 {
-                    if (trackedObject != null)
+                    if (_questionnaireToolkit != null)
                     {
-                        if (trackedObject != lastTrackedObject)
+                        if (_questionnaireToolkit != _lastQuestionnaireToolkit)
                         {
-                            trackedComponents = new bool[trackedObject.GetComponents(typeof(Component)).Length];
-                            trackedVariables = new bool[trackedObject.GetComponents(typeof(Component)).Length][];
+                            //_trackedComponents = new bool[_questionnaireToolkit.GetComponents(typeof(Component)).Length];
+                            //_trackedVariables = new bool[_questionnaireToolkit.GetComponents(typeof(Component)).Length][];
                         }
                         EditorGUILayout.LabelField("Select the components and variables you want to track:");
                         //Cycle through components of the tracked object
-                        for (int i = 0; i < trackedObject.GetComponents(typeof(Component)).Length; i++)
-                        {
-                            Component c = trackedObject.GetComponents(typeof(Component))[i];
-                            if (c != null)
-                            {
-                                EditorStyles.label.fontStyle = FontStyle.Bold;
-                                trackedComponents[i] = EditorGUILayout.Toggle(c.GetType().ToString(), trackedComponents[i]);
-                                EditorStyles.label.fontStyle = FontStyle.Normal;
-                                if (trackedObject != lastTrackedObject)
-                                {
-                                    trackedVariables[i] = new bool[c.GetType().GetProperties().Length + c.GetType().GetFields().Length];
-                                }
-                            }
+                        //for (int i = 0; i < _questionnaireToolkit.GetComponents(typeof(Component)).Length; i++)
+                        //{
+                        //    Component c = _questionnaireToolkit.GetComponents(typeof(Component))[i];
+                        //    if (c != null)
+                        //    {
+                        //        EditorStyles.label.fontStyle = FontStyle.Bold;
+                        //        _trackedComponents[i] = EditorGUILayout.Toggle(c.GetType().ToString(), _trackedComponents[i]);
+                        //        EditorStyles.label.fontStyle = FontStyle.Normal;
+                        //        if (_questionnaireToolkit != _lastQuestionnaireToolkit)
+                        //        {
+                        //            _trackedVariables[i] = new bool[c.GetType().GetProperties().Length + c.GetType().GetFields().Length];
+                        //        }
+                        //    }
 
-                            //Cycle through variables
-                            if (trackedComponents[i] == true)
-                            {
-                                EditorGUI.indentLevel++;
-                                for (int j = 0; j < c.GetType().GetProperties().Length; j++)
-                                {
-                                    var varToCheck = c.GetType().GetProperties()[j];
-                                    if (EventTypeChecker.IsValid(varToCheck.PropertyType))
-                                    {
-                                        trackedVariables[i][j] = EditorGUILayout.Toggle(varToCheck.Name, trackedVariables[i][j]);
-                                    }
-                                }
+                        //    //Cycle through variables
+                        //    if (_trackedComponents[i] == true)
+                        //    {
+                        //        EditorGUI.indentLevel++;
+                        //        for (int j = 0; j < c.GetType().GetProperties().Length; j++)
+                        //        {
+                        //            var varToCheck = c.GetType().GetProperties()[j];
+                        //            if (EventTypeChecker.IsValid(varToCheck.PropertyType))
+                        //            {
+                        //                _trackedVariables[i][j] = EditorGUILayout.Toggle(varToCheck.Name, _trackedVariables[i][j]);
+                        //            }
+                        //        }
 
-                                for (int j = c.GetType().GetProperties().Length; j < c.GetType().GetFields().Length + c.GetType().GetProperties().Length; j++)
-                                {
-                                    var varToCheck = c.GetType().GetFields()[j - c.GetType().GetProperties().Length];
-                                    if (EventTypeChecker.IsValid(varToCheck.FieldType))
-                                    {
-                                        trackedVariables[i][j] = EditorGUILayout.Toggle(varToCheck.Name, trackedVariables[i][j]);
-                                    }
-                                }
-                                EditorGUI.indentLevel--;
-                            }
-                        }
+                        //        for (int j = c.GetType().GetProperties().Length; j < c.GetType().GetFields().Length + c.GetType().GetProperties().Length; j++)
+                        //        {
+                        //            var varToCheck = c.GetType().GetFields()[j - c.GetType().GetProperties().Length];
+                        //            if (EventTypeChecker.IsValid(varToCheck.FieldType))
+                        //            {
+                        //                _trackedVariables[i][j] = EditorGUILayout.Toggle(varToCheck.Name, _trackedVariables[i][j]);
+                        //            }
+                        //        }
+                        //        EditorGUI.indentLevel--;
+                        //    }
+                        //}
                         if (GUILayout.Button("Generate Questionnaire"))
                         {
-                            CreateEvent();
-                            trackedObject = null;
+                            //CreateEvent();
+                            GenerateQuestionnaire();
+                            _questionnaireToolkit = null;
                         }
                     }
                     else
@@ -108,67 +110,74 @@ namespace VRSTK
                     }
 
 
-                    lastTrackedObject = trackedObject;
+                    _lastQuestionnaireToolkit = _questionnaireToolkit;
                 }
 
-                private void CreateEvent()
+                //private void CreateEvent()
+                //{
+                //    //Create Event itself
+                //    Telemetry.Event newEvent = (Telemetry.Event)ScriptableObject.CreateInstance("Event");
+                //    newEvent.eventName = _questionnaireToolkit.gameObject.name + _questionnaireToolkit.gameObject.GetInstanceID().ToString();
+
+                //    int numberOfProperties = 0;
+                //    int numberOfFields = 0;
+                //    List<string> savedNames = new List<string>();
+                //    for (int i = 0; i < _questionnaireToolkit.GetComponents(typeof(Component)).Length; i++)
+                //    {
+                //        Component c = _questionnaireToolkit.GetComponents(typeof(Component))[i];
+
+                //        //Cycle through variables
+                //        if (_trackedComponents[i] == true)
+                //        {
+                //            for (int j = 0; j < c.GetType().GetProperties().Length; j++)
+                //            {
+                //                if (_trackedVariables[i][j])
+                //                {
+                //                    savedNames.Add(string.Join("", new string[] { c.GetType().GetProperties()[j].Name, "_", c.GetType().Name }));
+                //                    numberOfProperties++;
+                //                }
+                //            }
+
+                //            for (int j = c.GetType().GetProperties().Length; j < c.GetType().GetFields().Length + c.GetType().GetProperties().Length; j++)
+                //            {
+                //                if (_trackedVariables[i][j])
+                //                {
+                //                    savedNames.Add(string.Join("", new string[] { c.GetType().GetFields()[j - c.GetType().GetProperties().Length].Name, "_", c.GetType().Name }));
+                //                    numberOfFields++;
+                //                }
+                //            }
+                //        }
+                //    }
+
+                //    try
+                //    {
+                //        TrackedObjects g = GameObject.Find("TrackedObjects").GetComponent<TrackedObjects>();
+                //        Undo.RegisterCompleteObjectUndo(g, "Added to tracked objects");
+                //        Undo.FlushUndoRecordObjects();
+                //        g.trackedObjects.Add(_questionnaireToolkit);
+                //        EditorUtility.SetDirty(g);
+                //    }
+                //    catch (NullReferenceException e)
+                //    {
+                //        Debug.LogError("The TrackedObjects GameObject was not found in the scene. Please add it to the scene from the prefabs folder. Exception message: " + e.Message);
+                //    }
+
+                //    //Attach Eventsender
+                //    EventSender s = _questionnaireToolkit.AddComponent<EventSender>();
+                //    s.eventBase = newEvent;
+                //    s.SetTrackedVar(_trackedComponents, _trackedVariables, savedNames);
+                //    AssetDatabase.CreateAsset(newEvent, "Assets/VRSTK/Events/Track" + _questionnaireToolkit.gameObject.name + _questionnaireToolkit.gameObject.GetInstanceID().ToString() + ".asset");
+                //    Undo.RecordObject(newEvent, "Created Event");
+                //    AssetDatabase.Refresh();
+                //    s.eventBase = (Telemetry.Event)AssetDatabase.LoadAssetAtPath("Assets/VRSTK/Events/Track" + _questionnaireToolkit.gameObject.name + _questionnaireToolkit.gameObject.GetInstanceID().ToString() + ".asset", typeof(Telemetry.Event));
+                //    AssetDatabase.SaveAssets();
+                //}
+
+                private void GenerateQuestionnaire()
                 {
-                    //Create Event itself
-                    Telemetry.Event newEvent = (Telemetry.Event)ScriptableObject.CreateInstance("Event");
-                    newEvent.eventName = trackedObject.gameObject.name + trackedObject.gameObject.GetInstanceID().ToString();
-
-                    int numberOfProperties = 0;
-                    int numberOfFields = 0;
-                    List<string> savedNames = new List<string>();
-                    for (int i = 0; i < trackedObject.GetComponents(typeof(Component)).Length; i++)
-                    {
-                        Component c = trackedObject.GetComponents(typeof(Component))[i];
-
-                        //Cycle through variables
-                        if (trackedComponents[i] == true)
-                        {
-                            for (int j = 0; j < c.GetType().GetProperties().Length; j++)
-                            {
-                                if (trackedVariables[i][j])
-                                {
-                                    savedNames.Add(string.Join("", new string[] { c.GetType().GetProperties()[j].Name, "_", c.GetType().Name }));
-                                    numberOfProperties++;
-                                }
-                            }
-
-                            for (int j = c.GetType().GetProperties().Length; j < c.GetType().GetFields().Length + c.GetType().GetProperties().Length; j++)
-                            {
-                                if (trackedVariables[i][j])
-                                {
-                                    savedNames.Add(string.Join("", new string[] { c.GetType().GetFields()[j - c.GetType().GetProperties().Length].Name, "_", c.GetType().Name }));
-                                    numberOfFields++;
-                                }
-                            }
-                        }
-                    }
-
-                    try
-                    {
-                        TrackedObjects g = GameObject.Find("TrackedObjects").GetComponent<TrackedObjects>();
-                        Undo.RegisterCompleteObjectUndo(g, "Added to tracked objects");
-                        Undo.FlushUndoRecordObjects();
-                        g.trackedObjects.Add(trackedObject);
-                        EditorUtility.SetDirty(g);
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        Debug.LogError("The TrackedObjects GameObject was not found in the scene. Please add it to the scene from the prefabs folder. Exception message: " + e.Message);
-                    }
-
-                    //Attach Eventsender
-                    EventSender s = trackedObject.AddComponent<EventSender>();
-                    s.eventBase = newEvent;
-                    s.SetTrackedVar(trackedComponents, trackedVariables, savedNames);
-                    AssetDatabase.CreateAsset(newEvent, "Assets/VRSTK/Events/Track" + trackedObject.gameObject.name + trackedObject.gameObject.GetInstanceID().ToString() + ".asset");
-                    Undo.RecordObject(newEvent, "Created Event");
-                    AssetDatabase.Refresh();
-                    s.eventBase = (Telemetry.Event)AssetDatabase.LoadAssetAtPath("Assets/VRSTK/Events/Track" + trackedObject.gameObject.name + trackedObject.gameObject.GetInstanceID().ToString() + ".asset", typeof(Telemetry.Event));
-                    AssetDatabase.SaveAssets();
+                    GenerateQuestionnaire q = _questionnaireToolkit.GetComponent<GenerateQuestionnaire>();
+                    q.PrepareConfigurations();
+                    q.InitializeQuestionnaire();
                 }
 
             }
