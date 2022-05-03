3fdac61f5b9dc44ad3ec77f7445604769e8885ed
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-05-03 13:15:16 +0200

    Updated OnRailLocomotionRoutes

--- Assets/OnRailLocomotionRoutes.cs
+++ Assets/OnRailLocomotionRoutes.cs
@@ -20,10 +20,15 @@ public class OnRailLocomotionRoutes : MonoBehaviour
 
     private bool _coroutineAllowed;
 
+    private float _time;
+
+    private float _xROriginYPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         _routeNumber = 0;
+        _xROriginYPosition = _xROrigin.position.y;
         _xROrigin.position = new Vector3(_routes[_routeNumber].GetChild(0).position.x, _xROrigin.position.y, _routes[_routeNumber].GetChild(0).position.z);
         _tParam = 0f;
         _coroutineAllowed = true;
@@ -32,8 +37,9 @@ public class OnRailLocomotionRoutes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _time += Time.deltaTime;
         bool testTageStarted = TestStage.GetStarted();
-        if (_coroutineAllowed && testTageStarted)
+        if (_coroutineAllowed && testTageStarted && _routeNumber < _routes.Length)
             StartCoroutine(OnRailLocomotion(_routeNumber));
     }
 
@@ -41,17 +47,18 @@ public class OnRailLocomotionRoutes : MonoBehaviour
     {
         _coroutineAllowed = false;
 
-        Vector3 cp0 = _routes[routeNumber].GetChild(0).position;
-        Vector3 cp1 = _routes[routeNumber].GetChild(1).position;
-        Vector3 cp2 = _routes[routeNumber].GetChild(2).position;
-        Vector3 cp3 = _routes[routeNumber].GetChild(3).position;
+        Vector3 cp0 = new Vector3(_routes[routeNumber].GetChild(0).position.x, _routes[routeNumber].GetChild(0).position.y + _xROriginYPosition, _routes[routeNumber].GetChild(0).position.z);
+        Vector3 cp1 = new Vector3(_routes[routeNumber].GetChild(1).position.x, _routes[routeNumber].GetChild(1).position.y + _xROriginYPosition, _routes[routeNumber].GetChild(1).position.z);
+        Vector3 cp2 = new Vector3(_routes[routeNumber].GetChild(2).position.x, _routes[routeNumber].GetChild(2).position.y + _xROriginYPosition, _routes[routeNumber].GetChild(2).position.z);
+        Vector3 cp3 = new Vector3(_routes[routeNumber].GetChild(3).position.x, _routes[routeNumber].GetChild(3).position.y + _xROriginYPosition, _routes[routeNumber].GetChild(3).position.z);
 
-        while(_tParam < 1)
+        while (_tParam < 1)
         {
             _tParam += Time.deltaTime * _speedModifier;
             Vector3 newPosition = Mathf.Pow(1 - _tParam, 3) * cp0 + 3 * _tParam * Mathf.Pow(1 - _tParam, 2) * cp1 + 3 * Mathf.Pow(_tParam, 2) * (1 - _tParam) * cp2 + Mathf.Pow(_tParam, 3) * cp3;
 
-            _xROrigin.position = new Vector3(newPosition.x, _xROrigin.position.y, newPosition.z);
+            _xROrigin.position = new Vector3(newPosition.x, newPosition.y, newPosition.z);
+
             yield return new WaitForEndOfFrame();
         }
 
@@ -64,4 +71,5 @@ public class OnRailLocomotionRoutes : MonoBehaviour
         else
             _coroutineAllowed = true;
     }
+
 }
