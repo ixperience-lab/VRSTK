3fe04f808cd6830cfd66346cf9f4273a0e7b2d8f
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-07-15 18:28:47 +0200

    Activated Bitalino tracker

--- Assets/VRSTK/Scripts/BiosignalsPlux/TrackingBitalinoWithOSC.cs
+++ Assets/VRSTK/Scripts/BiosignalsPlux/TrackingBitalinoWithOSC.cs
@@ -19,8 +19,17 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
 	[SerializeField]
 	public string _address = "";
 
-	[SerializeField]
+    [SerializeField]
+    public string _timeStamp = "";
+
+    [SerializeField]
 	public int _sequenceNumber = -1;
+
+	[SerializeField]
+	public string _rawReceivedMessage;
+
+	[SerializeField]
+	public string _transferedReceivedMessage;
 
 	[SerializeField]
 	public int[] _sigitalIOs = new int[4];
@@ -31,9 +40,7 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
 	[SerializeField]
 	public double[] _analogOutputsTransfered = new double[6];
 
-	[SerializeField]
-	public string _rawReceivedMessage;
-
+	
 	private Osc _handler;
 	private UDPPacketIO _udp;
 
@@ -56,29 +63,26 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		//Debug.Log("delta time in ms: " + Time.deltaTime.ToString());
 	}
 
-    private void FixedUpdate()
+    void OnDisable()
     {
-		//Debug.Log("fixed delta time in ms: " + Time.fixedDeltaTime.ToString());
+        if (_handler != null)
+		    _handler.Cancel();
+        if (_udp != null)
+		    _udp.Close();
 	}
-
- //   void OnDisable()
-	//{
-	//	_handler.Cancel();
-	//	_udp.Close();
-	//}
 
     private void OnApplicationQuit()
     {
-		_handler.Cancel();
-		_udp.Close();
-	}
+        if (_handler != null)
+            _handler.Cancel();
+        if (_udp != null)
+            _udp.Close();
+    }
 
     //These functions are called when messages are received
     //Access values via: oscMessage.Values[0], oscMessage.Values[1], etc
-
     public void AllMessageHandler(OscMessage oscMessage)
 	{
 		string msgString = Osc.OscMessageToString(oscMessage); //the message and value combined
@@ -87,8 +91,9 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
 		Debug.Log(msgString); //log the message and values coming from OSC
 		if (msgAddress.Equals(_address))
         {
-            //_test = new Thread(TrasnferRawValues(msgString));
-            //StartCoroutine(TrasnferRawValues(_rawReceivedMessage));
+            // message structure = "{0} - Seq[{1}] : O[{2} {3} {4} {5}] ; A[{6} {7} {8} {9} {10} {11}]"
+            _timeStamp = msgString.Split(' ')[1];
+
             int startIndex = msgString.IndexOf("Seq[") + 4;
             for (int i = 0; i < msgString.Length; i++)
                 if (msgString.Substring(startIndex + i, 1).Equals("]"))
@@ -112,8 +117,20 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
             for (int i = 0; i < msgString.Length; i++)
                 if (msgString.Substring(startIndex + i, 1).Equals("]"))
                 {
-                    _analogOutputs[0] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[0]);
-                    _analogOutputsTransfered[0] = _analogOutputs[0];
+                    // EDA (Electrodermal Activity) port A3
+                    {
+                        int eda_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[0]);
+                        _analogOutputs[0] = eda_raw;
+                        Debug.Log("A3 raw = " + eda_raw);
+                        // Transfer function [0uS, 25uS] (micro Siemens)
+                        int VCC = 3; // Operating voltage
+                        int ADC = eda_raw; // Value sampled form the channel
+                        int n = 10; // Number of bits of the channel
+                        float EDA_uS = (((float)ADC / (float)Math.Pow(2.0, (double)n)) * (float)VCC) / 0.12f;
+                        Debug.Log("A3 (micro Siemens) = " + EDA_uS);
+                        Debug.Log("A3 (Siemens) = " + (EDA_uS * Math.Pow(10.0, -6)));
+                        _analogOutputsTransfered[0] = EDA_uS;
+                    }
 
                     // ECG (Electrpcardiography)	
                     {
@@ -124,7 +141,7 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
                         {
                             int VCC = 3; // Operating voltage
                             int ADC = ecg_raw; // Value sampled form the channel
-                            int n = 8; // Number of bits of the channel
+                            int n = 10; // Number of bits of the channel
                             int gECG = 1019; // sensor gain
                             float ECG_V = ((((float)ADC / (float)Math.Pow(2.0, (double)n)) - 0.5f) * (float)VCC) / (float)gECG;
                             Debug.Log("A2 (Volt) = " + ECG_V);
@@ -133,21 +150,8 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
                         }
                     }
 
-                    // EDA (Electrodermal Activity) port A3
-                    {
-                        int eda_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[2]);
-                        _analogOutputs[2] = eda_raw;
-                        Debug.Log("A3 raw = " + eda_raw);
-                        // Transfer function [0uS, 25uS] (micro Siemens)
-                        int VCC = 3; // Operating voltage
-                        int ADC = eda_raw; // Value sampled form the channel
-                        int n = 8; // Number of bits of the channel
-                        float EDA_uS = (((float)ADC / (float)Math.Pow(2.0, (double)n)) * (float)VCC) / 0.12f;
-                        Debug.Log("A3 (micro Siemens) = " + EDA_uS);
-                        Debug.Log("A3 (Siemens) = " + (EDA_uS * Math.Pow(10.0, -6)));
-                        _analogOutputsTransfered[2] = EDA_uS;
-                    }
-
+                    _analogOutputs[2] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[2]);
+                    _analogOutputsTransfered[2] = _analogOutputs[2];
                     _analogOutputs[3] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[3]);
                     _analogOutputsTransfered[3] = _analogOutputs[3];
                     _analogOutputs[4] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[4]);
@@ -156,195 +160,11 @@ public class TrackingBitalinoWithOSC : MonoBehaviour
                     _analogOutputsTransfered[5] = _analogOutputs[5];
                     break;
                 }
+
+            _transferedReceivedMessage = string.Format("{0} {1} - Seq[{2}] : O[{3} {4} {5} {6}] ; A[{7} {8} {9} {10} {11} {12}]",
+                msgAddress, _timeStamp, _sequenceNumber.ToString(), _sigitalIOs[0] == 1 ? "true":"false", _sigitalIOs[1] == 1 ? "true" : "false", _sigitalIOs[2] == 1 ? "true" : "false", _sigitalIOs[3] == 1 ? "true" : "false",
+                _analogOutputsTransfered[0].ToString(), _analogOutputsTransfered[1].ToString(), _analogOutputsTransfered[2].ToString(), _analogOutputsTransfered[3].ToString(), _analogOutputsTransfered[4].ToString(), _analogOutputsTransfered[5].ToString());
         }
 	}
 
-	// Read Thread.  Loops waiting for packets.  When a packet is received, it is
-	// dispatched to any waiting All Message Handler.  Also, the address is looked up and
-	// any matching handler is called.
-	private void TrasnferRawValues(string msgString)
-	{
-		try
-		{
-			int startIndex = msgString.IndexOf("Seq[") + 4;
-			for (int i = 0; i < msgString.Length; i++)
-				if (msgString.Substring(startIndex + i, 1).Equals("]"))
-				{
-					_sequenceNumber = Convert.ToInt32(msgString.Substring(startIndex, i));
-					break;
-				}
-
-			startIndex = msgString.IndexOf("O[") + 2;
-			for (int i = 0; i < msgString.Length; i++)
-				if (msgString.Substring(startIndex + i, 1).Equals("]"))
-				{
-					_sigitalIOs[0] = msgString.Substring(startIndex, i).Split(' ')[0] == "True" ? 1 : 0;
-					_sigitalIOs[1] = msgString.Substring(startIndex, i).Split(' ')[1] == "True" ? 1 : 0;
-					_sigitalIOs[2] = msgString.Substring(startIndex, i).Split(' ')[2] == "True" ? 1 : 0;
-					_sigitalIOs[3] = msgString.Substring(startIndex, i).Split(' ')[3] == "True" ? 1 : 0;
-					break;
-				}
-
-			startIndex = msgString.IndexOf("A[") + 2;
-			for (int i = 0; i < msgString.Length; i++)
-				if (msgString.Substring(startIndex + i, 1).Equals("]"))
-				{
-					_analogOutputs[0] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[0]);
-					_analogOutputsTransfered[0] = _analogOutputs[0];
-
-					// ECG (Electrpcardiography)	
-					{
-						int ecg_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[1]);
-						Debug.Log("A2 raw = " + ecg_raw);
-						_analogOutputs[1] = ecg_raw;
-						// Transfer function [-1.47mV, +1.47mV] (micro Volt)
-						{
-							int VCC = 3; // Operating voltage
-							int ADC = ecg_raw; // Value sampled form the channel
-							int n = 8; // Number of bits of the channel
-							int gECG = 1019; // sensor gain
-							float ECG_V = ((((float)ADC / (float)Math.Pow(2.0, (double)n)) - 0.5f) * (float)VCC) / (float)gECG;
-							Debug.Log("A2 (Volt) = " + ECG_V);
-							Debug.Log("A2 (milli Volt) = " + (ECG_V * 1000));
-							_analogOutputsTransfered[1] = ECG_V;
-						}
-					}
-
-					// EDA (Electrodermal Activity) port A3
-					{
-						int eda_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[2]);
-						_analogOutputs[2] = eda_raw;
-						Debug.Log("A3 raw = " + eda_raw);
-						// Transfer function [0uS, 25uS] (micro Siemens)
-						int VCC = 3; // Operating voltage
-						int ADC = eda_raw; // Value sampled form the channel
-						int n = 8; // Number of bits of the channel
-						float EDA_uS = (((float)ADC / (float)Math.Pow(2.0, (double)n)) * (float)VCC) / 0.12f;
-						Debug.Log("A3 (micro Siemens) = " + EDA_uS);
-						Debug.Log("A3 (Siemens) = " + (EDA_uS * Math.Pow(10.0, -6)));
-						_analogOutputsTransfered[2] = EDA_uS;
-					}
-
-					_analogOutputs[3] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[3]);
-					_analogOutputsTransfered[3] = _analogOutputs[3];
-					_analogOutputs[4] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[4]);
-					_analogOutputsTransfered[4] = _analogOutputs[4];
-					_analogOutputs[5] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[5]);
-					_analogOutputsTransfered[5] = _analogOutputs[5];
-					break;
-				}
-
-			//while (ReaderRunning)
-			//{
-			//	byte[] buffer = new byte[1000];
-			//	int length = OscPacketIO.ReceivePacket(buffer);
-			//	//Debug.Log("received packed of len=" + length);
-			//	if (length > 0)
-			//	{
-			//		ArrayList messages = Osc.PacketToOscMessages(buffer, length);
-			//		foreach (OscMessage om in messages)
-			//		{
-			//			if (AllMessageHandler != null)
-			//				AllMessageHandler(om);
-			//			OscMessageHandler h = (OscMessageHandler)Hashtable.Synchronized(AddressTable)[om.Address];
-			//			if (h != null)
-			//				h(om);
-			//		}
-			//	}
-			//	else
-			//		Thread.Sleep(20);
-			//}
-		}
-		catch (Exception e)
-		{
-			//Debug.Log("ThreadAbortException"+e);
-		}
-		finally
-		{
-			//Debug.Log("terminating thread - clearing handlers");
-			//Cancel();
-			//Hashtable.Synchronized(AddressTable).Clear();
-		}
-
-	}
-
-	//IEnumerator TrasnferRawValues(string msgString)
-	//{
-	//	int startIndex = msgString.IndexOf("Seq[") + 4;
-	//	for (int i = 0; i < msgString.Length; i++)
-	//		if (msgString.Substring(startIndex + i, 1).Equals("]"))
-	//		{
-	//			_sequenceNumber = Convert.ToInt32(msgString.Substring(startIndex, i));
-	//			break;
-	//		}
-
-	//	startIndex = msgString.IndexOf("O[") + 2;
-	//	for (int i = 0; i < msgString.Length; i++)
-	//		if (msgString.Substring(startIndex + i, 1).Equals("]"))
-	//		{
-	//			_sigitalIOs[0] = msgString.Substring(startIndex, i).Split(' ')[0] == "True" ? 1 : 0;
-	//			_sigitalIOs[1] = msgString.Substring(startIndex, i).Split(' ')[1] == "True" ? 1 : 0;
-	//			_sigitalIOs[2] = msgString.Substring(startIndex, i).Split(' ')[2] == "True" ? 1 : 0;
-	//			_sigitalIOs[3] = msgString.Substring(startIndex, i).Split(' ')[3] == "True" ? 1 : 0;
-	//			break;
-	//		}
-
-	//	startIndex = msgString.IndexOf("A[") + 2;
-	//	for (int i = 0; i < msgString.Length; i++)
-	//		if (msgString.Substring(startIndex + i, 1).Equals("]"))
-	//		{
-	//			_analogOutputs[0] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[0]);
-	//			_analogOutputsTransfered[0] = _analogOutputs[0];
-
-	//			// ECG (Electrpcardiography)	
-	//			{
-	//				int ecg_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[1]);
-	//				Debug.Log("A2 raw = " + ecg_raw);
-	//				_analogOutputs[1] = ecg_raw;
-	//				// Transfer function [-1.47mV, +1.47mV] (micro Volt)
-	//				{
-	//					int VCC = 3; // Operating voltage
-	//					int ADC = ecg_raw; // Value sampled form the channel
-	//					int n = 8; // Number of bits of the channel
-	//					int gECG = 1019; // sensor gain
-	//					float ECG_V = ((((float)ADC / (float)Math.Pow(2.0, (double)n)) - 0.5f) * (float)VCC) / (float)gECG;
-	//					Debug.Log("A2 (Volt) = " + ECG_V);
-	//					Debug.Log("A2 (milli Volt) = " + (ECG_V * 1000));
-	//					_analogOutputsTransfered[1] = ECG_V;
-	//				}
-	//			}
-
-	//			// EDA (Electrodermal Activity) port A3
-	//			{
-	//				int eda_raw = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[2]);
-	//				_analogOutputs[2] = eda_raw;
-	//				Debug.Log("A3 raw = " + eda_raw);
-	//				// Transfer function [0uS, 25uS] (micro Siemens)
-	//				int VCC = 3; // Operating voltage
-	//				int ADC = eda_raw; // Value sampled form the channel
-	//				int n = 8; // Number of bits of the channel
-	//				float EDA_uS = (((float)ADC / (float)Math.Pow(2.0, (double)n)) * (float)VCC) / 0.12f;
-	//				Debug.Log("A3 (micro Siemens) = " + EDA_uS);
-	//				Debug.Log("A3 (Siemens) = " + (EDA_uS * Math.Pow(10.0, -6)));
-	//				_analogOutputsTransfered[2] = EDA_uS;
-	//			}
-
-	//			_analogOutputs[3] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[3]);
-	//			_analogOutputsTransfered[3] = _analogOutputs[3];
-	//			_analogOutputs[4] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[4]);
-	//			_analogOutputsTransfered[4] = _analogOutputs[4];
-	//			_analogOutputs[5] = Convert.ToInt32(msgString.Substring(startIndex, i).Split(' ')[5]);
-	//			_analogOutputsTransfered[5] = _analogOutputs[5];
-	//			break;
-	//		}
-
-	//	yield return null;
-	//	//Color c = renderer.material.color;
-	//	//for (float alpha = 1f; alpha >= 0; alpha -= 0.1f)
-	//	//{
-	//	//	c.a = alpha;
-	//	//	renderer.material.color = c;
-	//	//	yield return null;
-	//	//}
-	//}
 }
--- BitalinoTools/Tools/PythonWithOpenSignals/lsl_sample.py
+++ BitalinoTools/Tools/PythonWithOpenSignals/lsl_sample.py
@@ -19,15 +19,15 @@ class Lsl():
                 sample, timestamp = self.inlet.pull_sample()
                 print(timestamp, sample)
                 #{0} - Seq[{1}] : O[{2} {3} {4} {5}] ; A[{6} {7} {8} {9} {10} {11}]"
-                message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} 0.0 0.0 0.0 0.0 0.0 0.0]".format(timestamp, int(sample[0]), int(sample[1]))
+                message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} 0 0 0 0 0 0]".format(timestamp, int(sample[0]), int(sample[1]))
                 if len(sample) == 3:
-                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} 0.0 0.0 0.0 0.0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]))
+                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} 0 0 0 0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]))
                 if len(sample) == 4:
-                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} 0.0 0.0 0.0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]))
+                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} 0 0 0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]))
                 if len(sample) == 5:
-                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} {5} 0.0 0.0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]), int(sample[4]))
+                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} {5} 0 0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]), int(sample[4]))
                 if len(sample) == 6:
-                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} {5} {6} 0.0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]), int(sample[4]), int(sample[5]))
+                    message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} {5} {6} 0]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]), int(sample[4]), int(sample[5]))
                 if len(sample) == 7:
                     message = "{0} - Seq[{1}] : O[false false false false] ; A[{2} {3} {4} {5} {6} {7}]".format(timestamp, int(sample[0]), int(sample[1]), int(sample[2]), int(sample[3]), int(sample[4]), int(sample[5]), int(sample[6]))
                 print(message)
