3173b71202beeffc7345d99b6a106131d788aa43
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-09-01 11:43:16 +0200

    Added build automation script for the whole data fusion process; Updated cleaup environment script

---
+++ RTools/BuildAutomationProcess.R
@@ -0,0 +1,101 @@
+# automation build process
+
+# 0. Clear environment
+source("CleanUpEnvironmentFromTemporaryUsedVariables.r", echo=TRUE)
+
+# 1. ImportTrackingData
+source("ImportTrackingData.r", echo=TRUE)
+
+condition <- 'Condition B'
+type_vrstk <- 'VRSTK'
+type_biosppy <- 'Biosppy'
+id <- 'id-13'
+path <- file.path(condition,  type_vrstk, "/")
+
+# vrstk tracking files
+#rawTrackingData <- ImportTrackingData('Condition A/VRSTK/Proband-id-1-Condition-A_8-18_15-23-31.json')
+vrstk_files <- list.files(path, pattern=".json", all.files=T, full.names=T)
+for (file in vrstk_files) {
+  if(grepl(id, file)){
+    rawTrackingData <- ImportTrackingData(file)
+    break
+  }
+}
+
+# biosppy feature extracted file
+#transformedBilinoECGDataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0_id-1-Condition-A-EDA_EdaResults.txt')
+
+#transformedBilinoECGDataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-EDA_EdaResults.txt')
+
+#transformedBilinoECGDataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-EDA_EdaResults.txt')
+path <- file.path(condition,  type_biosppy, "/", id, "/")
+biosppy_files <- list.files(path, pattern=".txt", all.files=T, full.names=T)
+for (file in biosppy_files) {
+  if(grepl(id, file) && grepl("Stage-0", file) && grepl("ECG_HearRateResults", file)){
+    transformedBilinoECGDataFrameStage0 <- ImporttransformedBilinoTrackingData(file)
+  }
+  if(grepl(id, file) && grepl("Stage-1", file) && grepl("ECG_HearRateResults", file)){
+    transformedBilinoECGDataFrameStage1 <- ImporttransformedBilinoTrackingData(file)
+  }
+  if(grepl(id, file) && grepl("Stage-2", file) && grepl("ECG_HearRateResults", file)){
+    transformedBilinoECGDataFrameStage2 <- ImporttransformedBilinoTrackingData(file)
+  }
+  if(grepl(id, file) && grepl("Stage-0", file) && grepl("EDA_EdaResults", file)){
+    transformedBilinoEDADataFrameStage0 <- ImporttransformedBilinoTrackingData(file)
+  }
+  if(grepl(id, file) && grepl("Stage-1", file) && grepl("EDA_EdaResults", file)){
+    transformedBilinoEDADataFrameStage1 <- ImporttransformedBilinoTrackingData(file)
+  }
+  if(grepl(id, file) && grepl("Stage-2", file) && grepl("EDA_EdaResults", file)){
+    transformedBilinoEDADataFrameStage2 <- ImporttransformedBilinoTrackingData(file)
+  }
+}
+
+# 2. RawEmotivTrackingData
+source("RawEmotivTrackingData.r", echo=TRUE)
+
+# 3. RawBandPowerData
+source("RawBandPowerData.r", echo=TRUE)
+#   3.1 Downsampling/Upsampling
+
+# 4. RawPerformanceMetricData
+source("RawPerformanceMetricData.r", echo=TRUE)
+#   4.1 Downsampling/Upsampling
+
+# 5. PagesQualityParameters
+source("PagesQualityParameters.r", echo=TRUE)
+#   5.1 Downsampling/Upsampling
+
+# 6. RawFixationSaccadsData
+source("RawFixationSaccadesData.r", echo=TRUE)
+#   6.1 Downsampling/Upsampling
+
+# 7. RawVRQuestionnaireToolkitUncannyValleyData
+source("RawVRQuestionnaireToolkitUncannyValleyData.r", echo=TRUE)
+#   7.1 Downsampling/Upsampling
+
+# 8. RawVRQuestionnaireToolkitSSQDataFrame
+source("RawVRQuestionnaireToolkitSSQDataFrame.r", echo=TRUE)
+#   8.1 Downsampling to Seconds
+#   8.2 Calculate Quality Values
+
+# 9. Clear environment
+source("CleanUpEnvironmentFromTemporaryUsedVariables.r", echo=TRUE)
+
+#--------------------------------------------------
+# 10. Data-Fusion of one Participant
+
+#--------------------------------------------------
+# 11. Data-Fusion for all Probands
+
+#--------------------------------------------------
+# 12. Run Cluster-Algorithmen
+
+#--------------------------------------------------
+# 13. Run if there are results ANOVA
+
+
+
--- RTools/ImportTrackingData.R
+++ RTools/ImportTrackingData.R
@@ -15,7 +15,7 @@ ImportTrackingData <- function(filePath)
   return(rawTrackingData);
 }
 
-rawTrackingData <- ImportTrackingData('Condition A/VRSTK/Proband-id-1-Condition-A_8-18_15-23-31.json')
+#rawTrackingData <- ImportTrackingData('Condition A/VRSTK/Proband-id-1-Condition-A_8-18_15-23-31.json')
 
 ImporttransformedBilinoTrackingData <- function(filePath)
 {
@@ -27,14 +27,14 @@ ImporttransformedBilinoTrackingData <- function(filePath)
 }
 
 
-transformedBilinoECGDataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0-id-1-Condition-A-ECG_HearRateResults.txt')
-transformedBilinoEDADataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0_id-1-Condition-A-EDA_EdaResults.txt')
+#transformedBilinoECGDataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage0 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-0_id-1-Condition-A-EDA_EdaResults.txt')
 
-transformedBilinoECGDataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-ECG_HearRateResults.txt')
-transformedBilinoEDADataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-EDA_EdaResults.txt')
+#transformedBilinoECGDataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage1 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-1-id-1-Condition-A-EDA_EdaResults.txt')
 
-transformedBilinoECGDataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-ECG_HearRateResults.txt')
-transformedBilinoEDADataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-EDA_EdaResults.txt')
+#transformedBilinoECGDataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-ECG_HearRateResults.txt')
+#transformedBilinoEDADataFrameStage2 <- ImporttransformedBilinoTrackingData('Condition A/Biosppy/id-1/Bitalinoi-Proband-Stage-2-id-1-Condition-A-EDA_EdaResults.txt')
 
 
 
