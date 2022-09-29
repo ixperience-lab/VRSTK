f6e9cc96d95a25abc6fb19f550960244f83a031d
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-09-29 23:17:54 +0200

    Updated some scripts (add metrics)

--- PythonTools/K-MeansClustering.py
+++ PythonTools/K-MeansClustering.py
@@ -2,80 +2,138 @@
 # weblink: https://machinelearningmastery.com/clustering-algorithms-with-python/
 from numpy import unique
 from numpy import where
+import numpy as np
 from sklearn.datasets import make_classification
 from sklearn.cluster import MiniBatchKMeans
+from sklearn.preprocessing import StandardScaler
 # import KMeans
 from sklearn.cluster import KMeans
 from matplotlib import pyplot
-
+import seaborn as sns
+#sns.set_theme(color_codes=True)
+import os
 import pandas as pd
 
 # read csv input file
-input_data = pd.read_csv("All_Participents_DataFrame.csv", sep=";", decimal=',')
-#input_data = pd.read_csv("All_Participents_WaveSum_DataFrame.csv", sep=";", decimal=',')
+input_data = pd.read_csv("All_Participents_EEG_Clusterd_WaveSum_DataFrame.csv", sep=";", decimal=',')
 
-#print(input_data.head(1))
-#print(input_data.dtypes)
+# nomalize input data to create more varianz in the data
+#scaler = StandardScaler()
+
+input_x = input_data.drop(columns=['Conscientious', 'time', 'pId'])
+#print(input_x.head(1))
+
+#r_num = input_x.shape[0]
+#print(r_num)
+#c_num = input_x.shape[1]
+#print(c_num)
+
+#scaler.fit(input_x)
+#print(scaler.mean_)
+#transformed_input_x = scaler.transform(input_x)
+#print(transformed_input_x)
+#print(transformed_input_x[0])
 
 # define the model
-model = MiniBatchKMeans(n_clusters=2)
+#miniBatchKMeans = MiniBatchKMeans(n_clusters=2).fit(transformed_input_x)
+#input_score = miniBatchKMeans.score(transformed_input_x)
 
-input_data["Cluster"] = model.fit_predict(input_data)
-input_data["Cluster"] = input_data["Cluster"].astype("int")
-#print(input_data.head(1)) 
+miniBatchKMeans = MiniBatchKMeans(n_clusters=2).fit(input_x)
+input_score = miniBatchKMeans.score(np.array(input_x)[0:1])
+input_cluster_centers_ = miniBatchKMeans.cluster_centers_
+print(input_score)
+print(input_cluster_centers_)
 
-# fit the model
-""" model.fit(input_data)
-# assign a cluster to each example
-yhat = model.predict(input_data)
+#input_x["Cluster"] = miniBatchKMeans.predict(transformed_input_x)
+input_x["Cluster"] = miniBatchKMeans.predict(input_x)
+input_x["Cluster"] = input_x["Cluster"].astype("int")
 
-print(yhat)
+input_x["pId"] = input_data["pId"]
+input_x["ClusterFactor"] = pd.Series(data = np.zeros(input_x.shape[0]))
 
-# retrieve unique clusters
-clusters = unique(yhat)
-test = []
-#print(clusters[0])
-#print(clusters[1])
-# create scatter plot for samples from each cluster
-for cluster in clusters:
-	# get row indexes for samples with this cluster
-	row_ix = where(yhat == cluster)
-	test.append(row_ix)
-	print(row_ix)
-	# create scatter of these samples
-	#pyplot.scatter(input_data[row_ix, 0], input_data[row_ix, 1])
-	#pyplot.scatter(input_data[:, 0], input_data[:, 1], marker='o', s=2, c=C)
-# show the plot
-print(test) """
+_Ids = [ 1,2,3,4,5,6,7,10,13,14,15,16,17,18,19,20,31,34]
+for id in _Ids:
+	temp = input_x.loc[input_x["pId"] == id]
+	component_C0 =  temp[temp.Cluster == 0].shape[0]
+	component_C1 =  temp[temp.Cluster == 1].shape[0]
+	component_Sum = component_C0 + component_C1 
+	faktor_C0 = component_C0 / component_Sum
+	faktor_C1 = component_C1 / component_Sum
+	print(faktor_C0)
+	print(faktor_C1)
+	if faktor_C0 > faktor_C1: 
+		input_x.ClusterFactor[input_x.pId == id] = faktor_C0
+	if faktor_C0 < faktor_C1:  
+		input_x.ClusterFactor[input_x.pId == id] = faktor_C1
 
-ax2 = input_data.plot.scatter(x='Cluster',
-                       	      y='pId',
-                              c='Cluster',
-                              colormap='viridis')
-#pyplot.scatter(input_data[:, 0], input_data[:, 1], marker='o', c=yhat)
-#pyplot.scatter(test[0][0], test[1][0])
+ax2 = input_x.plot.scatter(x='pId',  y='ClusterFactor', c='Cluster', colormap='viridis')
 pyplot.show()
 
+# ------- pair (column_x_column) wise k-means cluster
 
+""" input_data = pd.read_csv("All_Participents_Clusterd_WaveSum_DataFrame.csv", sep=";", decimal=',')
+input_x = input_data.drop(columns=['Conscientious', 'time', 'pId'])
+print(input_x.head(1))
+r_num = input_x.shape[0]
+print(r_num)
+c_num = input_x.shape[1]
+print(c_num)
 
+# nomalize input data to create more varianz in the data
+scaler = StandardScaler()
+scaler.fit(input_x)
+transformed_input_x = scaler.transform(input_x)
+transformed_input_x = pd.DataFrame(data=transformed_input_x)
+
+miniBatchKMeans = MiniBatchKMeans(n_clusters=2)
+result_df = pd.DataFrame(columns=('column_index', 'compare_column_index', 'score', 'cluster_centers'))#, 'cluster_result'))
+for i in range(c_num):
+	c1 = transformed_input_x.iloc[:,i].values
+	print('======================================================= ' + str(i))
+	if i < (c_num - 1) :
+		for j in range(i+1,c_num):
+			print('=================================================================== ' + str(j))
+			c2 = transformed_input_x.iloc[:,j].values
+			tmp_input_x = pd.DataFrame(c1)#data=[c_1, c_2]).T
+			tmp_input_x[1] = c2
+			#break
+			miniBatchKMeans.fit(tmp_input_x)
+			tmp_score = miniBatchKMeans.score(tmp_input_x)
+			tmp_cluster_centers_ = miniBatchKMeans.cluster_centers_
+			#print(tmp_score)
+			#print(tmp_cluster_centers_)
+			#tmp_input_x["Cluster"] = miniBatchKMeans.predict(tmp_input_x)
+			#tmp_input_x["Cluster"] = tmp_input_x["Cluster"].astype("int")
+			result_df = pd.concat([result_df, pd.DataFrame({'column_index': i, 'compare_column_index' : j, 'score' : tmp_score, 'cluster_centers':  list(tmp_cluster_centers_)})])#, 'cluster_result': list(tmp_input_x["Cluster"].values)})])
+			#break	
+
+# mode
+mode = 0o666
+# with mode 0o666
+path = "./output/K-Means-Feature-Compare".format(i)
+if not os.path.exists(path):
+	os.mkdir(path, mode)
+generatet_file_name = "{}/Features_Analyse_with_Features{}".format(path, ".csv")
+result_df.to_csv(generatet_file_name, sep=";")
+ """
 #-----------------------------------------------------------------------------------------
 # read csv input file
-input_data = pd.read_csv("All_Participents_Mean_DataFrame.csv", sep=";", decimal=',')
+#input_data = pd.read_csv("All_Participents_Mean_DataFrame.csv", sep=";", decimal=',')
 #input_data = pd.read_csv("All_Participents_WaveSum_Mean_DataFrame.csv", sep=";", decimal=',')
 
 #print(input_data.head(1))
 #print(input_data.dtypes)
 
 # define the model
-model = MiniBatchKMeans(n_clusters=2)
+#model = MiniBatchKMeans(n_clusters=2)
 
-input_data["Cluster"] = model.fit_predict(input_data)
-input_data["Cluster"] = input_data["Cluster"].astype("int")
-print(input_data.head(1)) 
+#input_data["Cluster"] = model.fit_predict(input_data)
+#input_data["Cluster"] = input_data["Cluster"].astype("int")
+#print(input_data.head(1)) 
 
-ax2 = input_data.plot.scatter(x='Cluster', y='pId', c='Cluster', colormap='viridis')
+#ax2 = input_data.plot.scatter(x='Cluster', y='pId', c='Cluster', colormap='viridis')
 # show the plot
-pyplot.show()
+#pyplot.show()
 
 
 
--- PythonTools/LinearDiscriminantAnalysis_Clustering.py
+++ PythonTools/LinearDiscriminantAnalysis_Clustering.py
@@ -1,60 +1,151 @@
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import pandas as pd
 
 input_data = pd.read_csv("All_Participents_Clusterd_WaveSum_DataFrame.csv", sep=";", decimal=',')
 
+#input_data.set_index('time').plot()
+
+r_num = input_data.shape[0]
+print(r_num)
+c_num = input_data.shape[1]
+print(c_num)
+
+# nomalize input data to create more varianz in the data
+scaler = StandardScaler()
+
+for i in range(r_num):
+    if input_data["pId"].values[i] == 14 or input_data["pId"].values[i] == 15 or input_data["pId"].values[i] == 16:
+        input_data['Conscientious'].values[i] = 0
+
 load_test_data = pd.read_csv("All_Participents_Condition-C_WaveSum_DataFrame.csv", sep=";", decimal=',')
 
-#test_data = load_test_data.drop(columns=['time'])
-test_data = load_test_data
+test_data = load_test_data.drop(columns=['time', 'pId'])
+r_num_test_data = test_data.shape[0]
 test_x = test_data.iloc[:, :].values
+scaler.fit(test_x)
+#print(scaler.mean_)
+transformed_test_x = scaler.transform(test_x)
+
 
 #lda_x_data = input_data.drop(columns=['Conscientious', 'time'])
-lda_x_data = input_data.drop(columns=['Conscientious'])
+lda_x_data = input_data.drop(columns=['Conscientious', 'time', 'pId'])
 
-print(lda_x_data.head(1))
-print(test_data.head(1))
+#print(lda_x_data.head(1))
+#print(test_data.head(1))
 
-for col in test_data.columns:
-    found = False
-    for col1 in input_data.columns:
-        if col == col1:
-            found = True
-            break
-    if not found:
-        print(col)
 
 #------ Normalizing
 # Separating out the features
 x = lda_x_data.iloc[:, :].values
+scaler.fit(x)
+#print(scaler.mean_)
+transformed_x = scaler.transform(x)
 
 # Separating out the target
 y = np.array(input_data[["Conscientious"]].values.flatten()) #input_data[["Conscientious"]].values.to_numpy() #input_data.iloc[:,154].values#y = input_data.iloc[:,['Conscientious']].values
 
-lda = LinearDiscriminantAnalysis()
-lda.fit(x, y)
+lda = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
+lda.fit(transformed_x, y)
 
-result_array = lda.predict(x)
-print(result_array)
+#Define method to evaluate model
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 
-#print(lda.score(test_x, result_array))
-#print(lda.predict_proba(test_x))
-#print(lda.predict_log_proba(test_x))
+#evaluate model
+scores = cross_val_score(lda, transformed_x, y, scoring='accuracy', cv=cv, n_jobs=-1)
+print("=========================== evaluate model")
+print(scores)
+print(np.mean(scores))   
+
+df11=pd.DataFrame(lda.coef_[0].reshape(-1,1), lda_x_data.columns, columns=["Weight"])
+df12=pd.DataFrame(lda.intercept_[0].reshape(-1,1), ["Bias"], columns=["Weight"])
+resulty = pd.concat([df12, df11], axis=0)
+print("====================== fit informations")
+np.set_printoptions(threshold=np.inf)
+print(resulty)
+
+result_array = lda.predict(transformed_x)
+#print(result_array)
 
 lda_x_data['Conscientious'] = pd.Series(result_array)
 #lda_x_data['pId'] = input_data['pId']
-ax2 = lda_x_data.plot.scatter(x='Conscientious', y='pId', c='Conscientious', colormap='viridis')
+#ax2 = lda_x_data.plot.scatter(x='Conscientious', y='pId', c='Conscientious', colormap='viridis')
 # show the plot
-plt.show()
+#plt.show()
 
+print("================ transformend test validation input predictions informations")
+true_value_test_data = []
+test_data['pId'] = load_test_data['pId']
+#ids = [21, 22, 23, 24, 25, 26, 27, 28, 29]
+for i in range(r_num_test_data):
+    true_value_test_data.append([0])
+    if test_data['pId'].values[i] == 24 or test_data['pId'].values[i] == 25 or test_data['pId'].values[i] == 29:
+        true_value_test_data[i] = [1]
+#print(true_value_test_data)
 
-result_array = lda.predict(test_x)
-print(result_array)
+#test_data["PredictionScoreFactor_0"] = pd.Series(data = np.zeros(r_num_test_data))
+#test_data["PredictionScoreFactor_1"] = pd.Series(data = np.zeros(r_num_test_data))
+test_data["PredictionScoreFactor"] = pd.Series(data = np.zeros(r_num_test_data))
+result_array = []
+for i in range(r_num_test_data):
+    input_sample = transformed_test_x[1].reshape(1, -1)
+    predicted_result = lda.predict(input_sample)
+    result_array.append(predicted_result)
+    #calculatet_score = lda.score(input_sample, true_value_test_data[i])
+    #test_data.PredictionScoreFactor[i] = calculatet_score
+    #test_data.PredictionScoreFactor_0[i] = lda.predict_proba(input_sample)[0][0]
+    #test_data.PredictionScoreFactor_1[i] = lda.predict_proba(input_sample)[0][1]
+    #print(lda.predict_proba(input_sample))
+    #print(lda.predict_log_proba(input_sample))
+
+#print(result_array)
+
+print(accuracy_score(true_value_test_data, result_array))
+print(confusion_matrix(true_value_test_data, result_array))
+print(classification_report(true_value_test_data, result_array))
 
 test_data['Conscientious'] = pd.Series(result_array)
 #test_data['pId'] = load_test_data['pId']
+#ax2 = test_data.plot.scatter(x='PredictionScoreFactor_0', y='PredictionScoreFactor_1', c='Conscientious', colormap='viridis')
 ax2 = test_data.plot.scatter(x='Conscientious', y='pId', c='Conscientious', colormap='viridis')
 # show the plot
 plt.show()
+
+
+lda_roc_auc = roc_auc_score(true_value_test_data, lda.predict(transformed_test_x))
+fpr, tpr, thresholds = roc_curve(true_value_test_data, lda.predict_proba(transformed_test_x)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='LDA Model (area = %0.2f)' % lda_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('LDA_ROC')
+plt.show()
+
+#ids = [21, 22, 23, 24, 25, 26, 27, 28, 29]
+#for id in ids:
+#    temp = test_data.loc[test_data["pId"] == id]
+#    first =  temp[temp.Conscientious == 0].shape[0]
+#    second =  temp[temp.Conscientious == 1].shape[0]
+#    print(first)
+#    print(second)
+#    if first > second: 
+#        test_data.Conscientious[test_data.pId == id] = 0
+#    if first < second: 
+#        test_data.Conscientious[test_data.pId == id] = 1
+#ax2 = test_data.plot.scatter(x='Conscientious', y='pId', c='Conscientious', colormap='viridis')
+# show the plot
+#plt.show()
