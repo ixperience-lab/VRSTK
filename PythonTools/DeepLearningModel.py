06685a647ff90ab0ebe6e067944f0f3bb237268f
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-10-28 00:34:38 +0200

    Added CognetiveLoadEstimation script

--- PythonTools/DeepLearningModel.py
+++ PythonTools/DeepLearningModel.py
@@ -195,57 +195,57 @@ file_name = '{}/Transformed_True_test_data_plot.png'.format(path)
 plot_data_cluster(transformed_test_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
                  'Transformed (True) test data (True) test data plot', file_name, show=False, save=True)
 
-# print("------ T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) train data ")
-# # ------ T-Distributed Stochastic Neighbor Embedding n_components=2 of train data
-# tsne_model = TSNE(n_components=3, learning_rate=500.0 , init='pca', perplexity=30.0)
-# transformed_train_x = tsne_model.fit_transform(transformed_train_x)
-# print(transformed_train_x.shape)
-# conscientious_indeces = input_data.index[input_data['Conscientious'] == 0]
-# none_conscientious_indeces = input_data.index[input_data['Conscientious'] == 1]
-# file_name = '{}/tsne_True_train_data_plot.png'.format(path)
-# plot_data_cluster(transformed_train_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
-#                  'T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) train data  plot', file_name, show=False, save=True)
-
-# print("------ T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) test data")
-# # ------ T-Distributed Stochastic Neighbor Embedding n_components=2 of test data
-# transformed_test_x = tsne_model.fit_transform(transformed_test_x)
-# print(transformed_test_x.shape)
-# conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 0]
-# none_conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 1]
-# file_name = '{}/tsne_True_test_data_plot.png'.format(path)
-# plot_data_cluster(transformed_test_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
-#                  'T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) test data plot', file_name, show=False, save=True)
-
-print("------ Principal Component Analysis n_components=2 of train data")
-# ------ Principal Component Analysis n_components=2 of train data
-pca = PCA(n_components=3)
-transformed_train_x = pca.fit_transform(transformed_train_x)
-#print(pca.score(x)) # Debug only
-print(pca.explained_variance_ratio_)  # Debug only
-
+print("------ T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) train data ")
+# ------ T-Distributed Stochastic Neighbor Embedding n_components=2 of train data
+tsne_model = TSNE(n_components=3, learning_rate=500.0 , init='pca', perplexity=30.0)
+transformed_train_x = tsne_model.fit_transform(transformed_train_x)
+print(transformed_train_x.shape)
 conscientious_indeces = input_data.index[input_data['Conscientious'] == 0]
 none_conscientious_indeces = input_data.index[input_data['Conscientious'] == 1]
-plt.figure(figsize=(15,10))
-plt.scatter(transformed_train_x[conscientious_indeces.tolist(),0], transformed_train_x[conscientious_indeces.tolist(),1], c="b")
-plt.scatter(transformed_train_x[none_conscientious_indeces.tolist(),0], transformed_train_x[none_conscientious_indeces.tolist(),1], c="r")
-plt.title('Principal Component Analysis train data n_components=2 plot', fontsize=16)
-file_name = '{}/True_principal_components_train_data_plot.png'.format(path)
-plt.savefig(file_name)
-plt.close()
+file_name = '{}/tsne_True_train_data_plot.png'.format(path)
+plot_data_cluster(transformed_train_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
+                 'T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) train data  plot', file_name, show=False, save=True)
 
-transformed_test_x = pca.fit_transform(transformed_test_x)
-#print(pca.score(transformed_test_x)) # Debug only
-print(pca.explained_variance_ratio_)  # Debug only
-
+print("------ T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) test data")
+# ------ T-Distributed Stochastic Neighbor Embedding n_components=2 of test data
+transformed_test_x = tsne_model.fit_transform(transformed_test_x)
+print(transformed_test_x.shape)
 conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 0]
 none_conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 1]
-plt.figure(figsize=(15,10))
-plt.scatter(transformed_test_x[conscientious_indeces.tolist(),0], transformed_test_x[conscientious_indeces.tolist(),1], c="b")
-plt.scatter(transformed_test_x[none_conscientious_indeces.tolist(),0], transformed_test_x[none_conscientious_indeces.tolist(),1], c="r")
-plt.title('Principal Component Analysis test data n_components=2 plot', fontsize=16)
-file_name = '{}/True_principal_components_test_data_plot.png'.format(path)
-plt.savefig(file_name)
-plt.close()
+file_name = '{}/tsne_True_test_data_plot.png'.format(path)
+plot_data_cluster(transformed_test_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
+                 'T-Distributed Stochastic Neighbor Embedding n_components=2 of (True) test data plot', file_name, show=False, save=True)
+
+# print("------ Principal Component Analysis n_components=2 of train data")
+# # ------ Principal Component Analysis n_components=2 of train data
+# pca = PCA(n_components=3)
+# transformed_train_x = pca.fit_transform(transformed_train_x)
+# #print(pca.score(x)) # Debug only
+# print(pca.explained_variance_ratio_)  # Debug only
+
+# conscientious_indeces = input_data.index[input_data['Conscientious'] == 0]
+# none_conscientious_indeces = input_data.index[input_data['Conscientious'] == 1]
+# plt.figure(figsize=(15,10))
+# plt.scatter(transformed_train_x[conscientious_indeces.tolist(),0], transformed_train_x[conscientious_indeces.tolist(),1], c="b")
+# plt.scatter(transformed_train_x[none_conscientious_indeces.tolist(),0], transformed_train_x[none_conscientious_indeces.tolist(),1], c="r")
+# plt.title('Principal Component Analysis train data n_components=2 plot', fontsize=16)
+# file_name = '{}/True_principal_components_train_data_plot.png'.format(path)
+# plt.savefig(file_name)
+# plt.close()
+
+# transformed_test_x = pca.fit_transform(transformed_test_x)
+# #print(pca.score(transformed_test_x)) # Debug only
+# print(pca.explained_variance_ratio_)  # Debug only
+
+# conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 0]
+# none_conscientious_indeces = true_value_test_data.index[true_value_test_data['Conscientious'] == 1]
+# plt.figure(figsize=(15,10))
+# plt.scatter(transformed_test_x[conscientious_indeces.tolist(),0], transformed_test_x[conscientious_indeces.tolist(),1], c="b")
+# plt.scatter(transformed_test_x[none_conscientious_indeces.tolist(),0], transformed_test_x[none_conscientious_indeces.tolist(),1], c="r")
+# plt.title('Principal Component Analysis test data n_components=2 plot', fontsize=16)
+# file_name = '{}/True_principal_components_test_data_plot.png'.format(path)
+# plt.savefig(file_name)
+# plt.close()
 
 # ---- creates tensorflow tensors as shuffled datasets
 # shuffled_train_dataset = dataframe_to_dataset(train_dataframe) 
@@ -262,42 +262,8 @@ _train_data = train_data.copy()
 # train_dataframe, validation_dataframe = transformed_train_x[train_index], transformed_train_x[test_index]
 # y_train_true_output, y_validation_true_output = np.array(input_data["Conscientious"].values.flatten())[train_index], np.array(input_data["Conscientious"].values.flatten())[test_index]
 
-train_dataframe, validation_dataframe, y_train_true_output, y_validation_true_output = train_test_split(transformed_train_x, 
-                                    np.array(input_data["Conscientious"].values.flatten()), test_size=0.4,  shuffle=True, 
-                                    stratify=np.array(input_data["Conscientious"].values.flatten()), random_state=42)
-
-#validation_dataframe = x_train_data_frame.sample(frac=0.5, random_state=1337)
-#train_dataframe = x_train_data_frame.drop(validation_dataframe.index)
-#y_train_true_output = np.array(input_data.Conscientious.drop(validation_dataframe.index).values.flatten())
-#y_validation_true_output = np.array(input_data.Conscientious.values[validation_dataframe.index].flatten())
-print(y_train_true_output)
-print(y_validation_true_output)
-print("Using %d samples for training and %d for validation"  % (len(train_dataframe), len(validation_dataframe)))
-
-x_test_data_frame = pd.DataFrame(data=transformed_test_x)
-_test_data_copy = test_data.copy()
-
-# For Keras, convert dataframe to array values (Inbuilt requirement of Keras)
-X = train_dataframe.astype('float32')#.to_numpy(dtype='float32')
-print(X.shape)
-Y = y_train_true_output
-#Y = np_utils.to_categorical(Y, num_classes=2)
-print(Y.shape)
-
-v_X = validation_dataframe.astype('float32')#.to_numpy(dtype='float32')
-print(v_X.shape)
-v_Y = y_validation_true_output
-#v_Y = np_utils.to_categorical(v_Y, num_classes=2)
-print(v_Y.shape)
-#sys.exit()
-
-t_X = x_test_data_frame.to_numpy(dtype='float32')
-t_Y = np.array(true_value_test_data["Conscientious"].values.flatten())
-#t_Y = np_utils.to_categorical(t_Y, num_classes=2)
-print(t_X.shape)
-print(t_Y.shape)
-
 # ---- hyper parameters
+iterration = 100
 learning_rate = 0.001
 step_size = 10
 gamma = 0.9
@@ -307,78 +273,116 @@ num_epochs = 100
 patience = 10
 num_classes = 1
 
+aug = ClassificationSMOTE(p=0.2)
 # ---- tabnet
 tab_net_model = TabNetClassifier(optimizer_fn=torch.optim.Adam, optimizer_params=dict(lr=learning_rate), scheduler_params={"step_size":step_size, "gamma":gamma},
-                                 scheduler_fn=torch.optim.lr_scheduler.StepLR, mask_type='entmax')#, seed=42) # "sparsemax"#verbose=1, seed=42)
+                                 scheduler_fn=torch.optim.lr_scheduler.StepLR, mask_type='entmax')
+temp_path = path
+for iter in range(iterration):
+    path = "{}/iterration_{}".format(temp_path, iter)
+    if not os.path.exists(path):
+        os.mkdir(path, mode)
+    train_dataframe, validation_dataframe, y_train_true_output, y_validation_true_output = train_test_split(transformed_train_x, 
+                                        np.array(input_data["Conscientious"].values.flatten()), test_size=0.25,  shuffle=True, 
+                                        stratify=np.array(input_data["Conscientious"].values.flatten()))#, random_state=42)
 
-aug = ClassificationSMOTE(p=0.2)
+    #validation_dataframe = x_train_data_frame.sample(frac=0.5, random_state=1337)
+    #train_dataframe = x_train_data_frame.drop(validation_dataframe.index)
+    #y_train_true_output = np.array(input_data.Conscientious.drop(validation_dataframe.index).values.flatten())
+    #y_validation_true_output = np.array(input_data.Conscientious.values[validation_dataframe.index].flatten())
+    print(y_train_true_output)
+    print(y_validation_true_output)
+    print("Using %d samples for training and %d for validation"  % (len(train_dataframe), len(validation_dataframe)))
 
-tab_net_model.fit(X_train=X, y_train=Y, eval_set=[(X,Y),(v_X, v_Y)], eval_name=['train', 'valid'], max_epochs=num_epochs , patience=10, augmentations=aug,
-               batch_size=batch_size, virtual_batch_size=virtual_batch_size, eval_metric=['auc','accuracy'], drop_last=False)
+    x_test_data_frame = pd.DataFrame(data=transformed_test_x)
+    _test_data_copy = test_data.copy()
 
-predictions = tab_net_model.predict_proba(t_X)[:,1]
-print(predictions)
+    # For Keras, convert dataframe to array values (Inbuilt requirement of Keras)
+    X = train_dataframe.astype('float32')#.to_numpy(dtype='float32')
+    print(X.shape)
+    Y = y_train_true_output
+    #Y = np_utils.to_categorical(Y, num_classes=2)
+    print(Y.shape)
 
-predictions_transformed = []
-for i, predicted in enumerate(predictions):
-    #print(predicted)
-    if predicted > 0.7:
-        predictions_transformed.append(1)
-    else:
-        predictions_transformed.append(0)
+    v_X = validation_dataframe.astype('float32')#.to_numpy(dtype='float32')
+    print(v_X.shape)
+    v_Y = y_validation_true_output
+    #v_Y = np_utils.to_categorical(v_Y, num_classes=2)
+    print(v_Y.shape)
+    #sys.exit()
 
-# summarize history for accuracy
-plt.plot(tab_net_model.history['train_accuracy'])
-plt.plot(tab_net_model.history['valid_accuracy'])
-plt.title('tabnet accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-file_name = '{}/tabnet_history_accuracy_plot.png'.format(path)
-plt.savefig(file_name)
-plt.close()
+    t_X = x_test_data_frame.to_numpy(dtype='float32')
+    t_Y = np.array(true_value_test_data["Conscientious"].values.flatten())
+    #t_Y = np_utils.to_categorical(t_Y, num_classes=2)
+    print(t_X.shape)
+    print(t_Y.shape)
 
-_test_data_copy['Conscientious'] = predictions_transformed
-conscientious_indeces = _test_data_copy.index[_test_data_copy['Conscientious'] == 0]
-none_conscientious_indeces = _test_data_copy.index[_test_data_copy['Conscientious'] == 1]
-file_name = '{}/TabNetClassifier_Predicted_test_data_plot.png'.format(path)
-plot_data_cluster(transformed_test_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
-              'Tabnet predicted test data plot', file_name, show=False, save=True)
+    tab_net_model.fit(X_train=X, y_train=Y, eval_set=[(X,Y),(v_X, v_Y)], eval_name=['train', 'valid'], max_epochs=num_epochs , patience=10, augmentations=aug,
+                batch_size=batch_size, virtual_batch_size=virtual_batch_size, eval_metric=['auc','accuracy'], drop_last=False)
 
-# ------- display roc_auc curve
-lda_roc_auc = roc_auc_score(true_value_test_data["Conscientious"], predictions_transformed)
-fpr, tpr, thresholds = roc_curve(true_value_test_data["Conscientious"], predictions)#predictions[:,1])
-file_name = '{}/TabNetClassifier_DL-Model_test-data_ROC-curve.png'.format(path)
-plot_roc_curve(true_positive_rate = tpr, false_positive_rate = fpr, legend_label = 'DL-Model test data auc (area = %0.2f)' % lda_roc_auc, 
-               title = 'Tabnet-Model test data', file_name = file_name, show=False, save=True)
+    predictions = tab_net_model.predict_proba(t_X)[:,1]
+    print(predictions)
 
-precision, recall, thresholds = precision_recall_curve(true_value_test_data["Conscientious"], predictions)
-print(precision)
-print(recall)
-print(thresholds)
+    predictions_transformed = []
+    for i, predicted in enumerate(predictions):
+        #print(predicted)
+        if predicted > 0.7:
+            predictions_transformed.append(1)
+        else:
+            predictions_transformed.append(0)
 
-matrix = confusion_matrix(true_value_test_data['Conscientious'], predictions_transformed)
-print(matrix)
-file_name = '{}/TabNetClassifier_DL-Model_test_data_confusion_Matrix.txt'.format(path)
-write_matrix_and_report_to_file(file_name, np.array2string(matrix))
+    # summarize history for accuracy
+    plt.plot(tab_net_model.history['train_accuracy'])
+    plt.plot(tab_net_model.history['valid_accuracy'])
+    plt.title('tabnet accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    file_name = '{}/tabnet_history_accuracy_plot.png'.format(path)
+    plt.savefig(file_name)
+    plt.close()
 
-report = classification_report(true_value_test_data['Conscientious'], predictions_transformed)
-print(report)
-file_name = '{}/TabNetClassifier_DL-Model_test_deta_report.txt'.format(path)
-write_matrix_and_report_to_file(file_name, report)
+    _test_data_copy['Conscientious'] = predictions_transformed
+    conscientious_indeces = _test_data_copy.index[_test_data_copy['Conscientious'] == 0]
+    none_conscientious_indeces = _test_data_copy.index[_test_data_copy['Conscientious'] == 1]
+    file_name = '{}/TabNetClassifier_Predicted_test_data_plot.png'.format(path)
+    plot_data_cluster(transformed_test_x, conscientious_indeces.tolist(), none_conscientious_indeces.tolist(), 
+                'Tabnet predicted test data plot', file_name, show=False, save=True)
 
-f1_score_value = f1_score(true_value_test_data["Conscientious"], predictions_transformed, average=None)
-print(f1_score_value)
+    # ------- display roc_auc curve
+    lda_roc_auc = roc_auc_score(true_value_test_data["Conscientious"], predictions_transformed)
+    fpr, tpr, thresholds = roc_curve(true_value_test_data["Conscientious"], predictions)#predictions[:,1])
+    file_name = '{}/TabNetClassifier_DL-Model_test-data_ROC-curve.png'.format(path)
+    plot_roc_curve(true_positive_rate = tpr, false_positive_rate = fpr, legend_label = 'DL-Model test data auc (area = %0.2f)' % lda_roc_auc, 
+                title = 'Tabnet-Model test data', file_name = file_name, show=False, save=True)
 
-test_acc = accuracy_score(y_pred=true_value_test_data["Conscientious"], y_true=predictions_transformed)
-print(test_acc)
+    precision, recall, thresholds = precision_recall_curve(true_value_test_data["Conscientious"], predictions)
+    print(precision)
+    print(recall)
+    print(thresholds)
 
-display = PrecisionRecallDisplay.from_predictions(true_value_test_data["Conscientious"], predictions_transformed, name="DL-Model")
-_ = display.ax_.set_title("2-class Precision-Recall curve")
-file_name = '{}/TabNetClassifier_DL-Model_test-data_Precision-Recall-curve.png'.format(path)
-plt.savefig(file_name)
-plt.close()
+    matrix = confusion_matrix(true_value_test_data['Conscientious'], predictions_transformed)
+    print(matrix)
+    file_name = '{}/TabNetClassifier_DL-Model_test_data_confusion_Matrix.txt'.format(path)
+    write_matrix_and_report_to_file(file_name, np.array2string(matrix))
 
-# --- save tabnet model
-file_name = '{}/trained_tabnet_model'.format(path)
-saved_filepath = tab_net_model.save_model(file_name)
+    report = classification_report(true_value_test_data['Conscientious'], predictions_transformed)
+    print(report)
+    file_name = '{}/TabNetClassifier_DL-Model_test_deta_report.txt'.format(path)
+    write_matrix_and_report_to_file(file_name, report)
+
+    f1_score_value = f1_score(true_value_test_data["Conscientious"], predictions_transformed, average=None)
+    print(f1_score_value)
+
+    test_acc = accuracy_score(y_pred=true_value_test_data["Conscientious"], y_true=predictions_transformed)
+    print(test_acc)
+
+    display = PrecisionRecallDisplay.from_predictions(true_value_test_data["Conscientious"], predictions_transformed, name="DL-Model")
+    _ = display.ax_.set_title("2-class Precision-Recall curve")
+    file_name = '{}/TabNetClassifier_DL-Model_test-data_Precision-Recall-curve.png'.format(path)
+    plt.savefig(file_name)
+    plt.close()
+
+    # --- save tabnet model
+    file_name = '{}/trained_tabnet_model'.format(path)
+    saved_filepath = tab_net_model.save_model(file_name)
