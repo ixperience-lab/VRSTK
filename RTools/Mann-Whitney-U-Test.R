73d324fa8235d50eaf7c628076de3d3e0c87424a
HettmannWladimir <57088396+HettmannWladimir@users.noreply.github.com>
2022-12-05 23:20:22 +0100

    Updated font size in plots

--- RTools/Mann-Whitney-U-Test.R
+++ RTools/Mann-Whitney-U-Test.R
@@ -95,7 +95,11 @@ for (test_variable in c("theta", "alpha", "betaL", "betaH", "gamma", "eng", "exc
     xlab(label=test_variable) +
     ylab(label="density") +
     geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
-    theme(plot.title = element_text(hjust = 0.5)) +
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26)) +
     scale_x_continuous(limits = quantile(temp_df$t_variable, c(0.1, 0.9)))
   ggsave(path_png_file, width = 15, height = 10)
   
@@ -172,7 +176,11 @@ for (test_variable in c("HeartRate", "RPeaks", "RRI", "RRMin", "RRMean", "RRMax"
     xlab(label=test_variable) +
     ylab(label="density") +
     geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
-    theme(plot.title = element_text(hjust = 0.5)) #+
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26))#+
     #scale_x_continuous(limits = quantile(itemp_df$t_variable, c(0.1, 0.9)))
   ggsave(path_png_file, width = 15, height = 10)
   
@@ -259,7 +267,11 @@ for (test_variable in c("FilteredValueInMicroSiemens", "onsets", "peaks", "amps"
     xlab(label=test_variable) +
     ylab(label="density") +
     geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
-    theme(plot.title = element_text(hjust = 0.5)) #+
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26)) #+
     #scale_x_continuous(limits = quantile(itemp_df$t_variable, c(0.1, 0.9)))
   ggsave(path_png_file, width = 15, height = 10)
   
@@ -331,7 +343,11 @@ for (test_variable in c("TotalFixationCounter", "SaccadeCounter", "LeftPercentCh
     xlab(label=test_variable) +
     ylab(label="density") +
     geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
-    theme(plot.title = element_text(hjust = 0.5)) #+
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26)) #+
     #scale_x_continuous(limits = quantile(itemp_df$t_variable, c(0.1, 0.9)))
   ggsave(path_png_file, width = 15, height = 10)
   
@@ -348,6 +364,153 @@ for (test_variable in c("TotalFixationCounter", "SaccadeCounter", "LeftPercentCh
   ggsave(path_png_file, width = 15, height = 10)
   
 }
+
+# eye to visual impairment
+# ---------
+wilcoxon_subfolder_dir <- file.path("./wilcoxon_test", "eye_visual_impairment")
+if (!dir.exists(wilcoxon_subfolder_dir)){
+  dir.create(wilcoxon_subfolder_dir)
+}
+
+input_stage_1$LeftPercentChangePupilDialtion <- 0.0
+input_stage_1$RightPercentChangePupilDialtion <- 0.0
+
+visual_impairment_ids <- c( 1, 2, 3, 4, 6, 10, 13, 15, 17, 20, 34, 23, 27, 28)
+
+visual_impairment_stage_0 <- subset(input_stage_0, pId %in% visual_impairment_ids)#input_stage_0[input_stage_0$pId == visual_impairment_ids, ]
+visual_impairment_stage_1 <- subset(input_stage_1, pId %in% visual_impairment_ids)#input_stage_1[input_stage_1$pId == visual_impairment_ids, ]
+
+#seq <- c( 1, 2, 3, 4, 5, 6, 7, 10, 13, 14, 15, 16, 17, 18, 19, 20, 31, 34, 21, 22, 23, 24, 25, 26, 27, 28, 29 )
+for (i in visual_impairment_ids){
+  pId <- i
+  # stage 0
+  base_line_mean_l_pupil_diameter <- mean(visual_impairment_stage_0[visual_impairment_stage_0$pId == pId, ]$LeftPupilDiameter)
+  base_line_mean_r_pupil_diameter <- mean(visual_impairment_stage_0[visual_impairment_stage_0$pId == pId, ]$RightPupilDiameter)
+  # stage 1
+  visual_impairment_stage_1[visual_impairment_stage_1$pId == pId, ]$LeftPercentChangePupilDialtion  <- (visual_impairment_stage_1[visual_impairment_stage_1$pId == pId, ]$LeftPupilDiameter - base_line_mean_l_pupil_diameter) / base_line_mean_l_pupil_diameter
+  visual_impairment_stage_1[visual_impairment_stage_1$pId == pId, ]$RightPercentChangePupilDialtion  <- (visual_impairment_stage_1[visual_impairment_stage_1$pId == pId, ]$RightPupilDiameter - base_line_mean_r_pupil_diameter) / base_line_mean_r_pupil_diameter
+}
+
+for (test_variable in c("TotalFixationCounter", "SaccadeCounter", "LeftPercentChangePupilDialtion", "RightPercentChangePupilDialtion", "LeftPupilDiameter", 
+                        "RightPupilDiameter", "FixationCounter", "FixationDuration", "MeasuredVelocity", "SaccadesDiffX", "SaccadesMeanX", "SaccadesSdX", 
+                        "SaccadesMinX", "SaccadesMaxX", "CognitiveActivityLeftPupilDiamter", "CognitiveActivityLeftPupilDiamter", "CognitiveActivityRightPupilDiamter", 
+                        "LeftMeanPupilDiameter", "LeftPupilDiameterDifferenceToMean", "RightMeanPupilDiameter", "RightPupilDiameterDifferenceToMean")){
+  temp_column <- eval(call(name = "$", as.symbol("visual_impairment_stage_1"), as.symbol(test_variable)))
+  temp_df <- data.frame (t_variable  = temp_column, ValidityScore = visual_impairment_stage_1$ValidityScore, ValidityScore_Group=visual_impairment_stage_1$ValidityScore_Group)
+  
+  path_txt_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eye_",paste0(test_variable, "_results.txt")))
+  sink(path_txt_file)
+  
+  cat("======================================================================================================================================================\n")
+  cat(paste("EYE Variance test between test_variable: ", test_variable, " and Group: ValidityScore" ,"\n", sep=""))
+  cat("======================================================================================================================================================\n")
+  
+  print(ad.test(temp_df$t_variable))
+  
+  print(describeBy(t_variable ~ ValidityScore, data = temp_df))
+  
+  print(wilcox.test(temp_df$t_variable ~ temp_df$ValidityScore, exact = FALSE, correct = FALSE, conf.int = FALSE))
+  
+  print(temp_df %>%  wilcox_effsize(t_variable ~ ValidityScore))
+  
+  sink()
+  
+  path_png_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eye_",paste0(test_variable, "_hist_plot.png")))
+  ggplot(temp_df, aes(t_variable)) + 
+    geom_histogram(aes(y=..density..), color='gray50', fill='blue', alpha=0.2, position = "identity", outlier.shape = NA) +
+    #scale_x_continuous(limits = quantile(temp_df$t_variable, c(0.01, 0.99))) + 
+    labs(title=paste0(test_variable, " histogram plot"))  +
+    xlab(label=test_variable) +
+    ylab(label="density") +
+    geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26)) #+
+  #scale_x_continuous(limits = quantile(itemp_df$t_variable, c(0.1, 0.9)))
+  ggsave(path_png_file, width = 15, height = 10)
+  
+  path_png_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eye_",paste0(test_variable, "_box_plot.png")))
+  ggplot(temp_df, aes(x=ValidityScore, y=t_variable, fill=ValidityScore_Group, group=ValidityScore)) + 
+    stat_boxplot(geom = "errorbar", width = 0.25) +
+    geom_boxplot(outlier.shape = NA, outlier.size= NA, width = 0.5) + 
+    labs(title=paste0(test_variable, " box plot")) +
+    xlab(label="ValidityScore") +
+    ylab(label=test_variable) +
+    theme(legend.position="none") +
+    theme(plot.title = element_text(hjust = 0.5)) #+
+  #scale_y_continuous(limits = quantile(temp_df$t_variable, c(0.01, 0.99))) 
+  ggsave(path_png_file, width = 15, height = 10)
+  
+}
+
+
+
+# eeg to kaffee
+# ---------
+wilcoxon_subfolder_dir <- file.path("./wilcoxon_test", "eeg_to_kaffee")
+if (!dir.exists(wilcoxon_subfolder_dir)){
+  dir.create(wilcoxon_subfolder_dir)
+}
+
+kaffee_ids <- c( 1, 4, 6, 13, 15, 17, 18, 19, 20, 22, 26, 28, 29 )
+
+kaffee_stage_0 <- subset(input_stage_0, pId %in% visual_impairment_ids)#input_stage_0[input_stage_0$pId == visual_impairment_ids, ]
+kaffee_stage_1 <- subset(input_stage_1, pId %in% visual_impairment_ids)#input_stage_1[input_stage_1$pId == visual_impairment_ids, ]
+
+
+for (test_variable in c("theta", "alpha", "betaL", "betaH", "gamma", "eng", "exc", "str", "rel", "int", "foc")){
+  temp_column <- eval(call(name = "$", as.symbol("kaffee_stage_1"), as.symbol(test_variable)))
+  temp_df <- data.frame (t_variable  = temp_column, ValidityScore = kaffee_stage_1$ValidityScore, ValidityScore_Group=kaffee_stage_1$ValidityScore_Group)
+  
+  path_txt_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eeg_",paste0(test_variable, "_results.txt")))
+  sink(path_txt_file)
+  
+  cat("======================================================================================================================================================\n")
+  cat(paste("EEG Variance test between test_variable: ", test_variable, " and Group: ValidityScore" ,"\n", sep=""))
+  cat("======================================================================================================================================================\n")
+  
+  print(ad.test(temp_df$t_variable))
+  
+  print(describeBy(t_variable ~ ValidityScore, data = temp_df))
+  
+  print(wilcox.test(temp_df$t_variable ~ temp_df$ValidityScore, exact = FALSE, correct = FALSE, conf.int = FALSE))
+  
+  print(temp_df %>%  wilcox_effsize(t_variable ~ ValidityScore))
+  
+  sink()
+  
+  path_png_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eeg_",paste0(test_variable, "_hist_plot.png")))
+  ggplot(temp_df, aes(t_variable)) + 
+    geom_histogram(aes(y=..density..), color='gray50', fill='blue', alpha=0.2, position = "identity", outlier.shape = NA) +
+    scale_x_continuous(limits = quantile(temp_df$t_variable, c(0.01, 0.99))) + 
+    labs(title=paste0(test_variable, " histogram plot"))  +
+    xlab(label=test_variable) +
+    ylab(label="density") +
+    geom_density(alpha=0.2, color='red', fill='red', adjust=1, outlier.shape = NA)  +
+    theme(plot.title = element_text(hjust = 0.5, size = 28),
+          axis.text.x = element_text(size = 24),
+          axis.text.y = element_text(size = 24),  
+          axis.title.x = element_text(size = 26),
+          axis.title.y = element_text(size = 26)) +
+    scale_x_continuous(limits = quantile(temp_df$t_variable, c(0.1, 0.9)))
+  ggsave(path_png_file, width = 15, height = 10)
+  
+  path_png_file <- file.path(wilcoxon_subfolder_dir, paste0("wilcoxon_eeg_",paste0(test_variable, "_boxplot.png")))
+  ggplot(temp_df, aes(x=ValidityScore, y=t_variable, fill=ValidityScore_Group, group=ValidityScore)) + 
+    stat_boxplot(geom = "errorbar", width = 0.25) +
+    geom_boxplot(outlier.shape = NA, outlier.size= NA, width = 0.5) + 
+    labs(title=paste0(test_variable, " box plot")) +
+    xlab(label="ValidityScore") +
+    ylab(label=test_variable) +
+    theme(legend.position="none") +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    scale_y_continuous(limits = quantile(temp_df$t_variable, c(0.01, 0.99))) 
+  ggsave(path_png_file, width = 15, height = 10)
+}
+
+
 
 # Test version
 # ===================================
@@ -388,7 +551,11 @@ ggplot(input_ssq_ca, aes(mean)) +
   xlab(label="Condition A") +
   ylab(label="Density") +
   geom_density(alpha=0.2, color='red', fill='red', adjust=1)  +
-  theme(plot.title = element_text(hjust = 0.5)) #+
+  theme(plot.title = element_text(hjust = 0.5, size = 28),
+        axis.text.x = element_text(size = 24),
+        axis.text.y = element_text(size = 24),  
+        axis.title.x = element_text(size = 26),
+        axis.title.y = element_text(size = 26)) #+
 ggsave(path_png_file, width = 15, height = 10)
 
 path_png_file <- file.path(wilcoxon_subfolder_dir, "wilcoxon_ssq_cb_hist_plot.png")
@@ -398,7 +565,11 @@ ggplot(input_ssq_cb, aes(mean)) +
   xlab(label="Condition B") +
   ylab(label="Density") +
   geom_density(alpha=0.2, color='red', fill='red', adjust=1)  +
-  theme(plot.title = element_text(hjust = 0.5)) #+
+  theme(plot.title = element_text(hjust = 0.5, size = 28),
+        axis.text.x = element_text(size = 24),
+        axis.text.y = element_text(size = 24),  
+        axis.title.x = element_text(size = 26),
+        axis.title.y = element_text(size = 26)) #+
 ggsave(path_png_file, width = 15, height = 10)
 
 path_png_file <- file.path(wilcoxon_subfolder_dir, "wilcoxon_ssq_cb_hist_plot.png")
@@ -408,5 +579,9 @@ ggplot(input_ssq_cc, aes(mean)) +
   xlab(label="Condition B") +
   ylab(label="Density") +
   geom_density(alpha=0.2, color='red', fill='red', adjust=1)  +
-  theme(plot.title = element_text(hjust = 0.5)) #+
+  theme(plot.title = element_text(hjust = 0.5, size = 28),
+        axis.text.x = element_text(size = 24),
+        axis.text.y = element_text(size = 24),  
+        axis.title.x = element_text(size = 26),
+        axis.title.y = element_text(size = 26)) #+
 ggsave(path_png_file, width = 15, height = 10)
